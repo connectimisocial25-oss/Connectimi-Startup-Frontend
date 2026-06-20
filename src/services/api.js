68a8dec223f640bc03e1583a2308df6f67bdf041
connectimisocial25-oss <connectimi.social25@gmail.com>
2026-06-20 22:59:42 +0530

    Change API base URL to include version v1

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const url = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 const API = axios.create({
-  baseURL: `${url}/api/`,
+  baseURL: `${url}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
